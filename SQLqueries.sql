9b1d7b87802a69f886c987096274afb60a8d0fd3
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2025-06-30 19:02:41 +0100

    Update SQLqueries.sql

--- SQLqueries.sql
+++ SQLqueries.sql
@@ -1398,3 +1398,42 @@ BEGIN
 		PRINT '=================================================';
 	END CATCH
 END
+
+
+INSERT INTO silver.crm_cust_info(
+    cst_id,
+    cst_key,
+    cst_firstname,
+    cst_lastname,
+    cst_marital_status,
+    cst_gndr,
+    cst_create_date)
+SELECT
+       [cst_id]
+      ,[cst_key]
+      ,TRIM([cst_firstname]) AS cst_firstname
+      ,TRIM([cst_lastname]) AS cst_lastname
+      ,CASE UPPER(TRIM([cst_marital_status]))
+            WHEN 'M' THEN 'Married'
+            WHEN 'S' THEN 'Single'
+            ELSE 'n/a'
+       END AS cst_marital_status
+      ,CASE UPPER(TRIM([cst_gndr]))
+            WHEN 'M' THEN 'Male'
+            WHEN 'F' THEN 'Female'
+            ELSE 'n/a'
+       END AS cst_gndr
+      ,[cst_create_date]
+FROM
+(
+SELECT [cst_id]
+      ,[cst_key]
+      ,[cst_firstname]
+      ,[cst_lastname]
+      ,[cst_marital_status]
+      ,[cst_gndr]
+      ,[cst_create_date]
+      ,ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) AS row_id
+  FROM [Datawarehouse2].[bronze].[crm_cust_info])t
+  WHERE row_id = 1
+
