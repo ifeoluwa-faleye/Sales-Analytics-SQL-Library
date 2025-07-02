d5f87dd8009b1fa15ddbdd8eb5db70440d0186ff
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2025-07-02 18:24:17 +0100

    Update SQLqueries.sql

--- SQLqueries.sql
+++ SQLqueries.sql
@@ -1437,3 +1437,30 @@ SELECT [cst_id]
   FROM [Datawarehouse2].[bronze].[crm_cust_info])t
   WHERE row_id = 1
 
+INSERT INTO silver.crm_prd_info(
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+)
+SELECT
+	prd_id,
+	REPLACE(SUBSTRING(prd_key, 1, 5),'-', '_') AS cat_id,
+	SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+	prd_nm,
+	ISNULL(prd_cost, 0) AS prd_cost,
+	CASE UPPER(TRIM(prd_line))
+		WHEN 'R' THEN 'Road'
+		WHEN 'M' THEN 'Road'
+		WHEN 'S' THEN 'Other Sales'
+		WHEN 'T' THEN 'Touring'
+		ELSE 'n/a'
+	END AS prd_line,
+	CAST(prd_start_dt AS DATE) AS prd_start_dt,
+	DATEADD(day, -1, LEAD(CAST(prd_start_dt AS DATE)) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)) AS prd_end_dt
+FROM bronze.crm_prd_info;
+
