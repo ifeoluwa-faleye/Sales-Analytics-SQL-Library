1fc1c0a31d5d2005da1e58b2ab8ec4ad14647e09
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2025-07-08 18:31:47 +0100

    Update SQLqueries.sql

--- SQLqueries.sql
+++ SQLqueries.sql
@@ -1399,7 +1399,7 @@ BEGIN
 	END CATCH
 END
 
-
+--Transform and Insert into Silver Layer
 INSERT INTO silver.crm_cust_info(
     cst_id,
     cst_key,
@@ -1540,4 +1540,40 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
+
+--Transform and Insert Into Silver Layer
+INSERT INTO silver.crm_sales_details (
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+)
+SELECT
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	CASE WHEN sls_order_dt = 0 or LEN(sls_order_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE) 
+	END AS sls_order_dt,
+	CASE WHEN sls_ship_dt = 0 or LEN(sls_ship_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE) 
+	END AS sls_ship_dt,
+	CASE WHEN sls_due_dt = 0 or LEN(sls_due_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE) 
+	END AS sls_due_dt,
+	CASE WHEN sls_sales IS NULL or sls_sales <= 0 or sls_sales != sls_quantity * ABS(sls_price)
+		THEN sls_quantity * ABS(sls_price)
+	ELSE sls_sales
+	END AS sls_sales,
+	sls_quantity,
+	CASE WHEN sls_price IS NULL or sls_price <= 0
+		THEN sls_sales/NULLIF(sls_quantity,0)
+	ELSE sls_price
+	END AS sls_price
+FROM bronze.crm_sales_details
 
