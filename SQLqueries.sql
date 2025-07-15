aac6d61b8e2964d9f960a1f1b6fe3e09608b9d47
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2025-07-15 16:58:34 +0100

    Update SQLqueries.sql

--- SQLqueries.sql
+++ SQLqueries.sql
@@ -1577,3 +1577,23 @@ SELECT
 	END AS sls_price
 FROM bronze.crm_sales_details
 
+-- Transform bronze.erp_cust_az12 and load it into silver.erp_cust_az12
+
+INSERT INTO silver.erp_cust_az12(
+cid,
+bdate,
+gen
+)
+SELECT
+	CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+		ELSE cid
+	END AS cid,
+	CASE WHEN bdate > GETDATE() THEN NULL
+		ELSE bdate 
+	END AS bdate,
+	CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+		 WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+		 ELSE 'n/a'
+	END AS gen
+FROM bronze.erp_cust_az12;
+
