c7956dc4935efae86b965e45c2ae0c05360b19b3
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2025-07-16 20:06:34 +0100

    Update SQLqueries.sql

--- SQLqueries.sql
+++ SQLqueries.sql
@@ -1597,3 +1597,19 @@ SELECT
 	END AS gen
 FROM bronze.erp_cust_az12;
 
+
+-- Transform bronze.erp_loc_a101 and insert into Silver Layer
+
+INSERT INTO silver.erp_loc_a101(
+cid,
+cntry
+)
+
+SELECT 
+	REPLACE(cid,'-','') AS cid,
+	CASE WHEN UPPER(TRIM(cntry)) = 'DE' THEN 'Germany'
+		 WHEN UPPER(TRIM(cntry)) IN ('US', 'USA') THEN 'United States'
+		 WHEN UPPER(TRIM(cntry)) = '' OR UPPER(TRIM(cntry)) IS NULL THEN 'n/a'
+		 ELSE TRIM(cntry)
+	END AS cntry
+FROM bronze.erp_loc_a101;
