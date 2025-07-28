b91ff6cbcaadda940108c9eba448d2582da597fc
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2025-07-28 19:42:25 +0100

    Update SQLqueries.sql

--- SQLqueries.sql
+++ SQLqueries.sql
@@ -1881,3 +1881,23 @@ BEGIN
 		PRINT 'Error State' + CAST(ERROR_MESSAGE() AS NVARCHAR);
 	END CATCH
 END
+
+CREATE VIEW gold.dim_customers AS
+SELECT 
+	ROW_NUMBER() OVER(ORDER BY ci.cst_id) AS customer_key,
+	ci.cst_id AS customer_id,
+	ci.cst_key AS customer_number,
+	ci.cst_firstname AS firstname,
+	ci.cst_lastname AS lastname,
+	la.cntry AS country,
+	ci.cst_marital_status AS marital_status,
+	CASE WHEN ci.cst_gndr <> 'n/a' THEN ci.cst_gndr -- CRM is the Master for gender information
+		 ELSE COALESCE(ca.gen, 'n/a')
+	END AS gender,
+	ca.bdate AS birthdate,
+	ci.cst_create_date AS create_date
+FROM silver.crm_cust_info AS ci
+LEFT JOIN silver.erp_cust_az12 AS ca
+	ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 AS la
+ON ci.cst_key = la.cid;
