01783d739ff5cd00fd8fee2c4af7b2a0a053a764
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2025-12-05 21:21:41 +0000

    Update SQLqueries2.sql

--- SQLqueries2.sql
+++ SQLqueries2.sql
@@ -8,3 +8,26 @@ SELECT
 	AVG(Sales) OVER() AS avgsales,
 	AVG(Sales) OVER(PARTITION BY ProductID) AS avgsalesp
 FROM Sales.Orders;
+/*
+Retrieve all data from customers and orders as separate results
+*/
+SELECT *
+FROM SalesDB.Sales.Customers;
+SELECT *
+FROM SalesDB.Sales.Orders;
+
+/*
+Retrieve all data from customers that has a match in orders Table
+*/
+SELECT *
+FROM SalesDB.Sales.Customers AS c
+JOIN SalesDB.Sales.Orders AS o
+ON c.CustomerID = o.CustomerID;
+
+/*
+Retrieve all data from customers that has whether or not they have a match in orders Table
+*/
+SELECT *
+FROM SalesDB.Sales.Customers AS c
+LEFT JOIN SalesDB.Sales.Orders AS o
+ON c.CustomerID = o.CustomerID;
