43d5a39dc5597d9277141ddff1c6710c4b1bdec4
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-01-14 17:13:13 +0000

    Update SQLqueries2.sql

--- SQLqueries2.sql
+++ SQLqueries2.sql
@@ -443,3 +443,18 @@ BEGIN
         WHERE rnk = @N
     );
 END
+/* Write your T-SQL query statement below */
+WITH cte AS (
+    SELECT
+        num,
+        LAG(num)  OVER (ORDER BY id) AS prev_num,
+        LEAD(num) OVER (ORDER BY id) AS next_num
+    FROM Logs
+)
+SELECT DISTINCT
+    num AS ConsecutiveNums
+FROM
+    cte
+WHERE
+    num = prev_num
+    AND num = next_num;
