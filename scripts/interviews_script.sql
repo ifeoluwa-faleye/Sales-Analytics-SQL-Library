56559e5df28ce179bcc4ba195a3e7f4fc2e957c4
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-02-05 17:04:09 +0000

    Update interviews_script.sql

--- scripts/interviews_script.sql
+++ scripts/interviews_script.sql
@@ -1,1 +1,10 @@
+/*Show patient_id and first_name from patients 
+where their first_name start and ends with 's' 
+and is at least 6 characters long.*/
 
+SELECT
+	patient_id,
+    first_name
+FROM patients
+WHERE first_name LIKE ('s%s')
+AND LEN(first_name) >= 6
