39b3f750efa7bec61e91c3e5762b933ee5d4c62d
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-02-05 17:08:08 +0000

    Update interviews_script.sql

--- scripts/interviews_script.sql
+++ scripts/interviews_script.sql
@@ -8,3 +8,13 @@ SELECT
 FROM patients
 WHERE first_name LIKE ('s%s')
 AND LEN(first_name) >= 6
+/*Show patient_id, first_name, last_name from patients whos diagnosis is 'Dementia'.*/
+
+SELECT
+	p.patient_id,
+    p.first_name,
+    p.last_name
+FROM patients AS p
+JOIN admissions AS a
+ON p.patient_id = a.patient_id
+WHERE a.diagnosis = 'Dementia'
