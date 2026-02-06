f8976a0b95e3ad54a3e26e4afdd6094f1f12a3f1
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-02-06 16:48:12 +0000

    Update interviews_script.sql

--- scripts/interviews_script.sql
+++ scripts/interviews_script.sql
@@ -25,3 +25,13 @@ SELECT
     COUNT(patient_id) AS female_count
 FROM patients
 WHERE gender = 'F'
+/*Show first and last name, allergies from patients which have allergies to either 
+'Penicillin' or 'Morphine'. Show results ordered ascending 
+by allergies then by first_name then by last_name.*/
+SELECT
+	first_name,
+    last_name,
+    allergies
+FROM patients
+WHERE allergies IN ('Penicillin', 'Morphine')
+ORDER BY allergies, first_name, last_name
