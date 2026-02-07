fdbcf1136011cc64c4e60362d65943fceafcd327
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-02-07 15:16:32 +0000

    Update interviews_script.sql

--- scripts/interviews_script.sql
+++ scripts/interviews_script.sql
@@ -56,3 +56,16 @@ SELECT
 FROM patients
 GROUP BY city
 ORDER BY COUNT(patient_id) dESC, city
+/*Show first name, last name and role of every person that is either patient or doctor.
+The roles are either "Patient" or "Doctor"*/
+SELECT
+	first_name,
+    last_name,
+    'Patient' AS role
+FROM patients
+UNION ALL 
+SELECT
+	first_name,
+    last_name,
+    'Doctor'
+FROM doctors
