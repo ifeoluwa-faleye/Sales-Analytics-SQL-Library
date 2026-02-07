d18ce4d7b869d056aa794109572cf8d1e5ea8e75
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-02-07 15:10:09 +0000

    Update interviews_script.sql

--- scripts/interviews_script.sql
+++ scripts/interviews_script.sql
@@ -48,3 +48,11 @@ SELECT
     COUNT(diagnosis) OVER(PARTITION BY patient_id, diagnosis) AS diagnosis_count
 FROM admissions)t 
 WHERE diagnosis_count > 1
+/*Show the city and the total number of patients in the city.
+Order from most to least patients and then by city name ascending.*/
+SELECT
+	city,
+    COUNT(patient_id) AS num_patients
+FROM patients
+GROUP BY city
+ORDER BY COUNT(patient_id) dESC, city
