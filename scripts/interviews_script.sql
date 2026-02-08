ff68e22fe5276ab67a2944e6ce3751d79de29f05
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-02-08 15:42:32 +0000

    Update interviews_script.sql

--- scripts/interviews_script.sql
+++ scripts/interviews_script.sql
@@ -69,3 +69,16 @@ SELECT
     last_name,
     'Doctor'
 FROM doctors
+/*Show first name, last name and role of every person that is either patient or doctor.
+The roles are either "Patient" or "Doctor"*/
+SELECT
+	first_name,
+    last_name,
+    'Patient' AS role
+FROM patients
+UNION ALL 
+SELECT
+	first_name,
+    last_name,
+    'Doctor'
+FROM doctors
