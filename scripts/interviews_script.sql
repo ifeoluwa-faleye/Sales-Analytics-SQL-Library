523dd012207622271ad9774c9ad6f4442e89d584
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-02-12 17:50:06 +0000

    Update interviews_script.sql

--- scripts/interviews_script.sql
+++ scripts/interviews_script.sql
@@ -113,3 +113,8 @@ SELECT
 FROM patients
 GROUP BY province_id
 HAVING SUM(height) >= 7000
+/*Show the difference between the largest weight and smallest weight for patients with the last name 'Maroni'*/
+SELECT
+	MAX(weight)-MIN(weight)
+FROM patients
+WHERE last_name = 'Maroni'
