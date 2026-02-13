8610545f40d45e156fb7650242fb870e78472efa
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-02-13 18:12:31 +0000

    Update interviews_script.sql

--- scripts/interviews_script.sql
+++ scripts/interviews_script.sql
@@ -118,3 +118,8 @@ SELECT
 	MAX(weight)-MIN(weight)
 FROM patients
 WHERE last_name = 'Maroni'
+/*Show the difference between the largest weight and smallest weight for patients with the last name 'Maroni'*/
+SELECT
+	MAX(weight)-MIN(weight)
+FROM patients
+WHERE last_name = 'Maroni'
