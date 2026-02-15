c49f34feced0c1268e89f352932ffc858018f9c8
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-02-15 18:04:36 +0000

    Update interviews_script.sql

--- scripts/interviews_script.sql
+++ scripts/interviews_script.sql
@@ -131,3 +131,10 @@ SELECT
 FROM admissions
 GROUP BY DAY(admission_date)
 ORDER BY COUnT(patient_id) DESC
+/*Show all columns for patient_id 542's most recent admission_date.*/
+SELECT
+	*
+FROM admissions
+WHere patient_id = 542
+ORDER BY admission_date DESC
+LIMIT 1
