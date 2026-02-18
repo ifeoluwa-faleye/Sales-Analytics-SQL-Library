0a9ebc7edbe21489bcd8b68cba470d9fbcd2720b
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-02-18 17:21:33 +0000

    Update interviews_script.sql

--- scripts/interviews_script.sql
+++ scripts/interviews_script.sql
@@ -159,3 +159,13 @@ FROM doctors AS d
 JOIN admissions AS a
 ON d.doctor_id = a.attending_doctor_id
 GROUP BY d.first_name, d.last_name
+/*For each doctor, display their id, full name, and the first and last admission date they attended.*/
+SELECT
+	a.attending_doctor_id AS doctor_id,
+	CONCAT(d.first_name,' ',d.last_name) AS full_name,
+    MIN(a.admission_date) AS first_admission_date,
+    MAX(a.admission_date) AS last_admission_date
+FROM doctors AS d 
+JOIN admissions AS a 
+ON d.doctor_id = a.attending_doctor_id
+GROUP BY attending_doctor_id;
