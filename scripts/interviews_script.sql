46e37a6b1adadf637ed72a4daddfe92e146f267f
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-02-26 17:15:13 +0000

    Update interviews_script.sql

--- scripts/interviews_script.sql
+++ scripts/interviews_script.sql
@@ -237,3 +237,14 @@ SELECT
 	COUNT(patient_id) total_visits
 FROM admissions
 GROUP BY admission_date)t
+/*Display every patient that has at least one admission and show their most recent admission along with the patient and doctor's full name.*/
+SELECT
+	p.first_name||' '||p.last_name AS patient_name,
+    MAX(a.admission_date) AS admission_date,
+    d.first_name||' '||d.last_name AS doctor_name
+FROM patients AS p
+JOIN admissions AS a 
+	ON p.patient_id = a.patient_id
+JOIN doctors AS d 
+	ON a.attending_doctor_id = d.doctor_id
+GROUP BY p.patient_id;
