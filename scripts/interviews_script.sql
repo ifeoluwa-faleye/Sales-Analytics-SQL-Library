3dccb868dcd89411d12c3f4b69fcbc3a968ef7a2
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-02-27 20:04:39 +0000

    Update interviews_script.sql

--- scripts/interviews_script.sql
+++ scripts/interviews_script.sql
@@ -248,3 +248,30 @@ JOIN admissions AS a
 JOIN doctors AS d 
 	ON a.attending_doctor_id = d.doctor_id
 GROUP BY p.patient_id;
+/*Show all of the patients grouped into weight groups.
+Show the total amount of patients in each weight group.
+Order the list by the weight group decending.
+
+For example, if they weight 100 to 109 they are placed in the 100 weight group, 110-119 = 110 weight group, etc.*/
+SELECT
+	COUNT(patient_id),
+    CASE 
+        WHEN weight >=0 AND weight <10 THEN 0
+        WHEN weight >=10 AND weight <20 THEN 10
+        WHEN weight >=20 AND weight <30 THEN 20
+        WHEN weight >=30 AND weight <40 THEN 30
+        WHEN weight >=40 AND weight <50 THEN 40
+        WHEN weight >=50 AND weight <60 THEN 50
+        WHEN weight >=60 AND weight <70 THEN 60
+        WHEN weight >=70 AND weight <80 THEN 70
+        WHEN weight >=80 AND weight <90 THEN 80
+        WHEN weight >=90 AND weight <100 THEN 90
+        WHEN weight >=100 AND weight <110 THEN 100
+        WHEN weight >=110 AND weight <120 THEN 110
+        WHEN weight >=120 AND weight <130 THEN 120
+        WHEN weight >=130 AND weight <140 THEN 130
+        WHEN weight >=140 AND weight <150 THEN 140
+	END AS weight_group
+FROM patients
+GROUP BY weight_group
+ORDER BY weight_group DESC;
