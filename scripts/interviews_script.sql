8c4a78b1785b3c9a24b66e738443c4476e91bd1f
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-03-01 18:00:55 +0000

    Update interviews_script.sql

--- scripts/interviews_script.sql
+++ scripts/interviews_script.sql
@@ -275,3 +275,21 @@ SELECT
 FROM patients
 GROUP BY weight_group
 ORDER BY weight_group DESC;
+/*Show patient_id, weight, height, isObese from the patients table.
+
+Display isObese as a boolean 0 or 1.
+
+Obese is defined as weight(kg)/(height(m)2) >= 30.
+
+weight is in units kg.
+
+height is in units cm.*/
+SELECT
+	patient_id,
+    weight,
+    height,
+    CASE
+    	WHEN weight/(POWER(height/100.0,2)) >= 30 THEN 0
+        ELSE 1
+    END AS isObese
+FROM patients;
