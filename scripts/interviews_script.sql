f7a1298ed9fea740d820a0497a81d7ebe34f2c01
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-03-07 16:38:58 +0000

    Update interviews_script.sql

--- scripts/interviews_script.sql
+++ scripts/interviews_script.sql
@@ -382,3 +382,20 @@ SELECT
 	province_name
 FROM gender_tab 
 WHERE male_count > female_count
+/*We are looking for a specific patient. Pull all columns for the patient who matches the following criteria:
+- First_name contains an 'r' after the first two letters.
+- Identifies their gender as 'F'
+- Born in February, May, or December
+- Their weight would be between 60kg and 80kg
+- Their patient_id is an odd number
+- They are from the city 'Kingston'*/
+
+SELECT
+	*
+FROM patients
+WHERE first_name LIKE '__r%'
+AND gender = 'F'
+AND MONTH(birth_date) IN (2, 5, 12)
+AND (weight >=60 and weight <=80)
+AND patient_id % 2 <> 0
+AND city = 'Kingston'
