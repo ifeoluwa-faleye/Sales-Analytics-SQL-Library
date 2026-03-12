645de00dfccaf83fbd52ebeba6b30d9fd050b0c3
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-03-12 19:53:06 +0000

    Sort province names with 'Ontario' first
    Added a query to sort province names with 'Ontario' on top.

--- scripts/interviews_script.sql
+++ scripts/interviews_script.sql
@@ -431,3 +431,15 @@ SELECT distinct
 	province_name
 FROM province_names
 WHERE province_name <> 'Ontario';
+/*
+  Sort the province names in ascending order in such a way that the province 'Ontario' is always on top.
+*/
+SELECT
+	province_name
+FROM province_names
+WHERE province_name = 'Ontario'
+UNION ALL
+SELECT distinct
+	province_name
+FROM province_names
+WHERE province_name <> 'Ontario';
