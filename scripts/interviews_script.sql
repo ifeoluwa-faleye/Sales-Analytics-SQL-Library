e2d46bc2e06c6de478e683b0a1cd7dd7e595d14f
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-03-25 18:54:26 +0000

    Update interviews_script.sql

--- scripts/interviews_script.sql
+++ scripts/interviews_script.sql
@@ -592,3 +592,23 @@ FROM categories As ca
 JOIN products AS p 
 ON ca.category_id = p.category_id
 GROUP BY ca.category_name;
+/*
+Show the city, company_name, contact_name from the customers and suppliers table merged together.
+Create a column which contains 'customers' or 'suppliers' depending on the table it came from.
+*/
+
+SELECT
+    city,
+    company_name,
+    contact_name,
+    'customers' AS relationship
+FROM customers
+
+UNION
+
+SELECT
+    city,
+    company_name,
+    contact_name,
+    'suppliers' AS relationship
+FROM suppliers
