5915195f1e38e31616176733f24f56c4647bb430
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-03-26 19:08:16 +0000

    Add query for total orders by year and month
    Added a query to show total orders by year and month.

--- scripts/interviews_script.sql
+++ scripts/interviews_script.sql
@@ -612,3 +612,13 @@ SELECT
     contact_name,
     'suppliers' AS relationship
 FROM suppliers
+/*
+Show the total amount of orders for each year/month.
+*/
+
+SELECT
+    YEAR(order_date) AS order_year,
+    MONTH(order_date) AS order_month,
+    COUNT(order_id) AS no_of_orders
+FROM orders
+GROUP BY order_year, order_month
