f606ac551a46152fa2afc37c6980dad67c448a37
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-03-29 15:35:45 +0100

    Add SQL query to show total orders by year/month

--- scripts/interviews_script.sql
+++ scripts/interviews_script.sql
@@ -622,3 +622,13 @@ SELECT
     COUNT(order_id) AS no_of_orders
 FROM orders
 GROUP BY order_year, order_month
+/*
+Show the total amount of orders for each year/month.
+*/
+
+SELECT
+    YEAR(order_date) AS order_year,
+    MONTH(order_date) AS order_month,
+    COUNT(order_id) AS no_of_orders
+FROM orders
+GROUP BY order_year, order_month
